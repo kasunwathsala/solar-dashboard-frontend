56712c7fc5a9fb367933b0311676dd0286b00785
kasunwathsala <wathsala.nilaweera2001@gmail.com>
2025-11-21 14:58:20 +0530

    feat: Refactor AdminSidebar to use new sidebar components and improve layout structure

--- src/layouts/admin.layout.jsx
+++ src/layouts/admin.layout.jsx
@@ -1,28 +1,17 @@
 import { Outlet } from "react-router";
-import AdminSidebar from "@/components/AdminSidebar";
+import { AdminSidebar } from "@/components/AdminSidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function AdminLayout() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <AdminSidebar />
-      <div className="ml-64 flex flex-1 flex-col">
-        <main className="flex-1 pb-8">
-          <div className="bg-white shadow">
-            <div className="px-4 sm:px-6 lg:max-w-6xl lg:mx-auto lg:px-8">
-              <div className="py-6 md:flex md:items-center md:justify-between lg:border-t lg:border-gray-200">
-                <div className="min-w-0 flex-1">
-                  {/* Page header content will be rendered by individual pages */}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="mt-8">
-            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-              <Outlet />
-            </div>
-          </div>
+    <>
+      <SidebarProvider>
+        <AdminSidebar />
+        <main className="p-4 w-full bg-slate-200">
+          <SidebarTrigger className="block" />
+          <Outlet />
         </main>
-      </div>
-    </div>
+      </SidebarProvider>
+    </>
   );
 }
