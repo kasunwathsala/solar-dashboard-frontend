a07f0ef0ebd7113b8f6e6061d469ec49c3074998
kasunwathsala <wathsala.nilaweera2001@gmail.com>
2026-01-20 10:16:42 +0530

    updated

--- src/pages/home/components/HeroSection/HeroSection.jsx
+++ src/pages/home/components/HeroSection/HeroSection.jsx
@@ -53,7 +53,7 @@ export default function HeroSection() {
                 <span className="bg-gradient-to-r from-green-500 to-green-600 bg-clip-text text-transparent">Solar Energy</span>
                 <div className="relative">
                   <img
-                    src="/images/ddd.jpg"
+                    src="/ddd.jpg"
                     alt="Solar panels on a house roof"
                     className="max-h-8 rounded-xl object-cover sm:max-h-16 md:max-h-20 md:rounded-2xl shadow-lg"
                   />
